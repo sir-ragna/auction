ce6b6f9ecbefcb074744b2f6e3b0172e7320a5b9
Robbe Van der Gucht <sir.ragna@gmail.com>
2021-12-13 21:24:01 +0100

    rebuild db

--- auction/models.py
+++ auction/models.py
@@ -6,6 +6,9 @@ class Listing(models.Model):
     description = models.CharField(max_length=280, null=False)
     date_created = models.DateTimeField(auto_now_add=True, null=False)
     date_modified = models.DateTimeField(auto_now=True, null=False)
+    starting_bid = models.PositiveIntegerField(null=False)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    
     def __str__(self):
         return f"{self.id}:{self.title}"
     def __repr__(self):
